adfa69a659cf8d37ca4fcec842fbc1f0c836528f
WooRaZil-Boy <byuncorry@hanmail.net>
2019-01-13 02:20:31 +0900

    finished Ch1_Setting Up Android Studio

--- Android_Apprentice/Timefighter/app/src/main/java/com/example/geunseong_gai/timefighter/GameActivity.kt
+++ Android_Apprentice/Timefighter/app/src/main/java/com/example/geunseong_gai/timefighter/GameActivity.kt
@@ -10,3 +10,21 @@ class GameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_game)
     }
 }
+
+
+//Setting up an Android device p.38
+//실제 안드로이드 디바이스를 연결하려면, 기기의 설정에서 Setting > About Phone 에서 build number를 눌러 메시지가 뜨도록 해야 한다.
+//그러면 Setting에서 Developer Options 탭이 활성화되는데, USB Debugging을 토글해 설정해 줘야 한다.
+
+
+
+
+//Running the app
+//project navigator에서 app ‣ res ‣ layout ‣ activity_game.xml을 클릭하면 레이아웃을 볼 수 있다.
+//하단 탭의 Design | Text 를 선택해 전환할 수 있다.
+
+
+
+
+//Installing new versions of Android studio
+//Preferences ‣ Appearance & Behavior ‣ System Settings ‣ Android SDK 에서 최신 SDK를 관리한다.
